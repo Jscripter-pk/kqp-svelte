c77d9fa4cb936bdcb20aeae80f9dfd47d114885d
jscripter-pk <islam.bscs3275@iiu.edu.pk>
2026-02-25 22:10:14 +0500

    chore: quick fix to show demo via ngrok

--- vite.config.ts
+++ vite.config.ts
@@ -2,5 +2,14 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-	plugins: [sveltekit()]
+	plugins: [sveltekit()],
+	server: {
+		allowedHosts: true, // This allows any host to connect
+		proxy: {
+			'/apik': {
+				target: 'http://141.164.63.141',
+				changeOrigin: true
+			}
+		}
+	}
 });
